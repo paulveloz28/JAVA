3156c9bc5544cc8282913918c1ecd2794c9555aa
paulveloz28 <paulveloz28@gmail.com>
2026-08-06 22:18:15 -0500

    feat: Agregar funcion multiplicar a la clase Calculadora

---
+++ Clases/Clases/src/com/krakedev/Calculadora.java
@@ -0,0 +1,30 @@
+package com.krakedev;
+
+public class Calculadora {
+	
+	public int sumar(int a, int b) {
+		  
+	    int resultado;
+	    resultado = a + b;
+	    return resultado;
+
+	  }
+
+	  public int restar(int a, int b) {
+	  
+	    int resultado;
+	    resultado = a - b;
+	    return resultado;
+	  }
+	  
+	  public double multiplicar(double valor1, double valor2) {
+
+			double resultado;
+			resultado = valor1 * valor2;
+			return resultado;
+
+	  }
+
+}
+
+
---
+++ Clases/Clases/src/com/krakedev/TestCalculadora.java
@@ -0,0 +1,29 @@
+package com.krakedev;
+
+public class TestCalculadora {
+
+	public static void main(String[] args) {
+
+		
+		Calculadora c1 = new Calculadora();
+
+	      int r1;
+	      
+	      r1 = c1.sumar(4, 2);
+
+	      System.out.println("Resultado Suma: " + r1);
+
+	      int r2;
+	      
+	      r2 = c1.restar(4, 2);
+
+	      System.out.println("Resultado Resta: " + r2);
+	      
+	      double r3;
+	      r3 = c1.multiplicar(10, 5);
+	      System.out.println("Resultado Multiplicacion: " + r3);
+	      
+
+	}
+
+}
