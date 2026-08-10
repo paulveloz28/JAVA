2c811abbad072aac17d214b4256cc68d17b560ce
paulveloz28 <paulveloz28@gmail.com>
2026-08-10 14:49:31 -0500

    feat: Crear ejercicio clase Fecha

---
+++ Clases/Clases/src/com/krakedev/Fecha.java
@@ -0,0 +1,28 @@
+package com.krakedev;
+
+public class Fecha {
+	
+	private int dia;
+	private int mes;
+	private int anio;
+	
+	public int getDia() {
+		return dia;
+	}
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+	public int getMes() {
+		return mes;
+	}
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+	public int getAnio() {
+		return anio;
+	}
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+	
+}
---
+++ Clases/Clases/src/com/krakedev/TestFecha.java
@@ -0,0 +1,20 @@
+package com.krakedev;
+
+public class TestFecha {
+
+	public static void main(String[] args) {
+		
+		
+		Fecha f1 = new Fecha();
+		
+		f1.setAnio(2026);
+		f1.setMes(4);
+		f1.setDia(1);
+		
+		
+		System.out.println("Año: " + f1.getAnio());
+		System.out.println("Mes: " + f1.getMes());
+		System.out.println("Dia: " + f1.getDia());
+	}
+
+}
